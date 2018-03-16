5152845a4aef411a91df331e49a2d68776dc98a0
mmusicoc <mariomusicortes@gmail.com>
2018-03-16 01:42:35 +0100

    Data creator works!

--- Datainput.c
+++ Datainput.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void swap(int * a, int * b) {
+  int aux = *a;
+  *a = *b;
+  *b = aux;
+}
+
 void showvector(int * datos, int longitud){
 	int i;
 	for (i = 0; i < longitud; i++){
@@ -8,46 +14,84 @@ void showvector(int * datos, int longitud){
 	printf("\n");
 }
 
-void fillvector(int * datos, int longitud){
+void startvector(int * datos, int longitud){
 	int i;
 	for (i = 0; i < longitud; i++){
 		datos[i]=i+1;
 	}
 }
 
-void swap(int * a, int * b) {
-  int aux = *a;
-  *a = *b;
-  *b = aux;
+void invertvector(int * datos, int longitud){
+	int i, j;
+	for (i = 0, j = longitud - 1; i < j; i++, j--){
+		swap(datos + i, datos + j);
+	}
 }
 
 void randomize(int * datos, int longitud){
-	int i, j, k;
+	int i, j;
 	srand(clock());
-	for(k = 0; k < longitud; k++){
-		srand(clock());
+	for(j = 0; j < longitud; j++){
 		for(i=0; i < longitud; i++){
-			j = rand() % longitud;
-			swap(datos + i, datos + j);
+			swap(datos + i, datos + (rand() % longitud));
 		}
 	}
 }
 
-int datacreator(char modo, int * datos, int longitud){
-	int i;
-	fillvector(datos, longitud);
-	for(i = 0; i < 20; i++){
-		randomize(datos, longitud);
-		showvector(datos, longitud);
+void duplicate(int * datos, int longitud){
+	int i, j;
+	srand(clock());
+	j = rand() % longitud;
+	j < 2? 2: j;
+	for(i = 0; i < j; i++){
+		*(datos + rand()%longitud) = *(datos + rand()%longitud);
 	}
+}
+
+void inputdata(int * datos, int longitud){
+	int i;
+	for(i = 0; i < longitud; i++){
+		printf("Introduzca dato %d: ", i+1);
+		scanf("%d", datos + i);
+	}
+}
+
+int datacreator(char modo, int * datos, int longitud){
+	startvector(datos, longitud);
+	switch(modo){
+		case 'a':
+			break;
+		case 'b': 
+			invertvector(datos, longitud);
+			break;
+		case 'c':
+			randomize(datos, longitud);
+			break;
+		case 'd':
+			duplicate(datos, longitud);
+			break;
+		case 'e':
+			inputdata(datos, longitud);
+			break;
+	}
+	showvector(datos, longitud);
 }
 
 int main() {
 	int longitud;
-	printf("Intruduzca longitud:");
+	char modo;
+	printf("\nMODOS DISPONIBLES:\n");
+	printf("a) Ordenado sentido creciente\n");
+	printf("b) Ordenado sentido decreciente\n");
+	printf("c) Desordenado\n");
+	printf("d) Con duplicados\n");
+	printf("e) Introducir datos personalizados\n\n");
+	printf("Intruduzca modo: ");
+	scanf("%c", &modo);
+	printf("Cantidad de datos a ordenar: ");
 	scanf("%d", &longitud);
 	int datos[longitud];
-	datacreator('a', datos, longitud);
-	
+	datacreator(modo, datos, longitud);
+
 	return 0;
 }
